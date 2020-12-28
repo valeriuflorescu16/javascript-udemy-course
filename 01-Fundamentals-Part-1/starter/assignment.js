a3a773f02b9e3925e05d965cf6cb3030fc13cdc1
valeriuflorescu16 <valeriuflorescu16@users.noreply.github.com>
2020-12-28 20:46:39 +0000

    Update assignment.js

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -105,6 +105,9 @@ if (dolphinsAverage > koalaAverage && dolphinsBool) {
 }
 */
 
+/*
+let bill = 430;
+let tip = bill >= 50 && bill <= 300 ? (15 * bill) / 100 : (20 * bill) / 100;
 
-
-
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}.`);
+*/
