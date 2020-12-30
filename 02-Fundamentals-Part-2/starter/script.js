73b29f39b17b8e09515d8bd6c7de07a3d99754fc
valeriuflorescu16 <valeriuflorescu16@users.noreply.github.com>
2020-12-30 14:12:36 +0000

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -79,6 +79,7 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(3, 7));
 */
 
+/*
 const calcAge = function (birthYear) {
   return 2021 - birthYear;
 }
@@ -98,5 +99,117 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1999, 'Valeriu'));
 console.log(yearsUntilRetirement(1950, 'Adi'));
+*/
+
+/*
+const friend1 = `Michael`;
+const friend2 = `Stephen`;
+const friend3 = `Josh`;
+
+const friends = [`Michael`, `Stephen`, `Josh`];
+console.log(friends);
+
+//const years = new Array(1991, 1984, 1999, 2020);
+//console.log(years);
+
+console.log(friends[0]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = `Joshua`;
+console.log(friends);
+
+const valeriu = [`Valeriu`, `Florescu`, 2021 - 1999, friends];
+console.log(valeriu);
+console.log(valeriu.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+  return 2021 - birthYear;
+}
+const years = new Array(1991, 1984, 1999, 2020);
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), age2, age3];
+console.log(ages);
+*/
+
+/*
+//add elements to array
+const friends = [`Michael`, `Stephen`, `Josh`];
+const newLength = friends.push(`Dan`);
+console.log(friends);
+console.log(newLength);
+
+friends.unshift(`Vali`); //also returns the new length of array
+console.log(friends);
+
+//Remove elements
+friends.pop(); //last
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+friends.shift(); //first
+console.log(friends);
+
+const indexOfStephen = friends.indexOf(`Stephen`);
+console.log(indexOfStephen);
+
+friends.push(23);
+console.log(friends.includes(`Stephen`));
+console.log(friends.includes(`Vali`));
+console.log(friends.includes(`23`));
+console.log(friends.includes(23));
 
 
+if (friends.includes(`Stephen`)) {
+  console.log(`You have a friend called Stephen`);
+}
+*/
+
+/*
+// Objects
+const valeriu = {
+
+  firstName: `Valeriu`,
+  lastName: `Florescu`,
+  age: 2021 - 1999,
+  job: `Student`,
+  friends: [`Vali`, `Josh`, `Dan`]
+
+};
+console.log(valeriu);
+console.log(valeriu.lastName);
+console.log(valeriu['lastName']);
+
+const nameKey = `Name`;
+console.log(valeriu['first' + nameKey]);
+console.log(valeriu['last' + nameKey]);
+
+const interestedIn = prompt(`What do you want to know about Valeriu? Choose between firstName, lastName, age, job and friends.`);
+console.log(interestedIn);
+
+if (valeriu[interestedIn]) {
+  console.log(valeriu[interestedIn]);
+} else {
+  console.log(`Wrong request. Choose between firstName, lastName, age, job and friends.`);
+}
+
+valeriu.location = `United Kingdom`;
+valeriu[`originCountry`] = `Romania`;
+console.log(valeriu);
+*/
+
+//Challenge
+
+const valeriu = {
+
+  friends: ['Vali', 'Josh', 'Sergiu'],
+  bestFriend: friends[1]
+
+};
