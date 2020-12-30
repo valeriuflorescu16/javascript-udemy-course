e3dd663a1af39bc42f9ebd0a6e20cfb5b301dd66
valeriuflorescu16 <valeriuflorescu16@users.noreply.github.com>
2020-12-30 14:12:27 +0000

    Create assignment.js

---
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/*
+// average function to calculate the average for each team
+const calcAverage = (s1, s2, s3) => (s1 + s2 + s3) / 3;
+
+//calculating the average using the average function
+let averageDolphins = calcAverage(44, 23, 71);
+let averageKoalas = calcAverage(65, 54, 49);
+
+// function used to find out the winner team
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    return `Dolphins win by ${avgDolphins - avgKoalas} points on average`;
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    return `Koalas win by ${avgKoalas - avgDolphins} points on average`;
+  } else {
+    return `No team wins`;
+  }
+}
+
+console.log(checkWinner(averageDolphins, averageKoalas));
+
+averageDolphins = calcAverage(85, 54, 41);
+averageKoalas = calcAverage(23, 34, 27);
+
+console.log(checkWinner(averageDolphins, averageKoalas));
+*/
+
+/*
+function calcTip(bill) {
+  let x;
+  if (bill >= 50 && bill <= 300) {
+    x = (15 / 100) * bill;
+  } else {
+    x = (20 / 100) * bill;
+  }
+  return x;
+}
+
+console.log(calcTip(100));
+
+const bills = new Array(125, 555, 44);
+
+const t1 = calcTip(bills[0]);
+const t2 = calcTip(bills[1]);
+const t3 = calcTip(bills[2]);
+
+const tips = new Array(t1, t2, t3);
+
+const totals = new Array(bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]);
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
+*/
